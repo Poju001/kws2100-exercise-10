a814272a0f5131a1314e4492c7dc0dca47145913
Poi Juchai <poju001@student.kristiania.no>
2026-03-16 16:27:32 +0100

    creating postgis api in hono

--- server/server.ts
+++ server/server.ts
@@ -1,9 +1,33 @@
 import { Hono } from "hono";
 import { serve } from "@hono/node-server";
 import { serveStatic } from "@hono/node-server/serve-static";
+import pg from "pg";
+
+const postgresql = new pg.Pool({
+  connectionString:
+    process.env.DATABASE_URL || "postgresql://postgres:@localhost",
+});
 
 const app = new Hono();
-// `serveStatic` makes Hono serve the output from `vite build`
-app.get("*", serveStatic({ root: "../dist" }));
-app.get("/", (c) => c.text("hello world"));
+
+app.get("/api/skoler", async (c) => {
+  const result = await postgresql.query(
+    "select organisasjonsnummer, skolenavn, st_transform(posisjon, 4326)::json as geometry from grunnskole",
+  );
+
+  return c.json({
+    type: "FeatureCollection",
+    crs: { type: "name", properties: { name: "ESPG:4326" } },
+    features: result.rows.map(
+      ({ geometry: { coordinates, type }, ...properties }) => ({
+        type: "Feature",
+        geometry: { type, coordinates },
+        properties,
+      }),
+    ),
+  });
+});
+
+app.use("*", serveStatic({ root: "../dist" }));
+
 serve(app);
